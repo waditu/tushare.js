e3e8b4c1a0a78f3a38158515d887e1484d9e9932
ruanyl <ruanyu1@gmail.com>
2016-01-09 23:07:32 +0200

    one more test for classifying details

--- test/classifyingDetailsTest.js
+++ test/classifyingDetailsTest.js
@@ -1,9 +1,21 @@
 var tushare = require('../lib/index');
 var test = require('tape');
 
-test('Get Day Price', function(t) {
+test('Get default classifying Details', function(t) {
   t.plan(2);
   tushare.stock.getSinaClassifyDetails(function(err, data) {
+    t.ok(Object.prototype.toString.apply(data) === '[object Array]', 'It should return an array of stocks of an industry');
+    t.ok(data.length > 0, 'It should return more than one stocks in an industry');
+  });
+});
+
+test('Get specified classifying Details', function(t) {
+  t.plan(2);
+  var options = {
+    tag: 'gn_zndw'
+  };
+  tushare.stock.getSinaClassifyDetails(options, function(err, data) {
+    console.log(data);
     t.ok(Object.prototype.toString.apply(data) === '[object Array]', 'It should return an array of stocks of an industry');
     t.ok(data.length > 0, 'It should return more than one stocks in an industry');
   });
