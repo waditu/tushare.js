628b8cced5bbb39e3a9dede42d5a21a008377520
ruanyl <ruanyu1@gmail.com>
2016-01-11 23:36:55 +0200

    remove console.log

--- test/classifyingAllStocksTest.js
+++ test/classifyingAllStocksTest.js
@@ -4,7 +4,6 @@ var test = require('tape');
 test('Get All Stock Data', function(t) {
   t.plan(2);
   tushare.stock.getAllStocks(function(err, data) {
-    console.log(data);
     t.ok(Object.prototype.toString.apply(data) === '[object Array]', 'It should return an array of stocks');
     t.ok(data.length > 0, 'It should return more than one stocks');
   });
