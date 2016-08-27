345cadd551fdb4e15ce0b785ebc323a3f442ce87
ruanyl <ruanyu1@gmail.com>
2016-08-27 13:41:15 +0300

    use fetch instead of superagent

--- src/stock/billboard.js
+++ src/stock/billboard.js
@@ -1,11 +1,9 @@
 import request from 'superagent-charset';
+import 'isomorphic-fetch';
 import moment from 'moment';
-import {
-  lhbUrl,
-  blockTradeUrl,
-  longPeriodRankUrl
-} from './urls';
-import { codeToSymbol } from './util';
+import { lhbUrl, blockTradeUrl, longPeriodRankUrl } from './urls';
+import { codeToSymbol, checkStatus } from './util';
+import { DATE_NOW } from './cons.js';
 
 /**
  * lhb - 获取龙虎榜数据
@@ -95,53 +93,40 @@ export function lhb(options, cb) {
  * @param cb
  * @returns {undefined}
  */
-export function blockTrade(options, cb) {
+export function blockTrade(_options = {}) {
   const defaults = {
-    start: moment().format('YYYY-MM-DD'),
-    end: moment().format('YYYY-MM-DD'),
+    start: DATE_NOW,
+    end: DATE_NOW,
     pageNo: 1,
-    pageSize: 150
+    pageSize: 150,
   };
-  if(Object.prototype.toString.apply(options) === '[object Function]') {
-    cb = options;
-    options = {};
-  }
-  options = Object.assign(defaults, options);
+  const options = Object.assign({}, defaults, _options);
   const url = blockTradeUrl(options.start, options.end, options.pageNo, options.pageSize);
+  const mapData = data => {
+    const result = {};
+    result.page = data.page + 1;
+    result.total = data.total;
+    result.pageCount = data.pagecount;
+    result.items = data.list.map(item => ({
+      symbol: codeToSymbol(item.SYMBOL),
+      name: item.SNAME,
+      dealPrice: item.DZJY5,
+      price: item.TCLOSE,
+      date: item.PUBLISHDATE,
+      volume: item.DZJY2 * 100,
+      amount: item.DZJY6,
+      buyer: item.DZJY9,
+      seller: item.DZJY11,
+      dopRate: item.DZJY55,
+    }));
+    return { data: result };
+  };
 
-  request
-    .get(url)
-    .charset('gbk')
-    .buffer()
-    .end(function(err, res) {
-      if(err || !res.ok) {
-        cb(err);
-      } else if(res.text) {
-        let result = {};
-        let data = JSON.parse(res.text);
-        result.page = data.page + 1;
-        result.total = data.total;
-        result.pageCount = data.pagecount;
-        result.items = data.list.map((item) => {
-          return {
-            symbol: codeToSymbol(item.SYMBOL),
-            name: item.SNAME,
-            dealPrice: item.DZJY5,
-            price: item.TCLOSE,
-            date: item.PUBLISHDATE,
-            volume: item.DZJY2 * 100,
-            amount: item.DZJY6,
-            buyer: item.DZJY9,
-            seller: item.DZJY11,
-            dopRate: item.DZJY55
-          };
-        });
-
-        cb(null, result);
-      } else {
-        cb(null, {});
-      }
-    });
+  return fetch(url)
+  .then(checkStatus)
+  .then(res => res.json())
+  .then(mapData)
+  .catch(error => ({ error }));
 }
 
 /**
--- src/stock/cons.js
+++ src/stock/cons.js
@@ -1,5 +1,23 @@
-export const K_TYPE = {'day': 'akdaily', 'week': 'akweekly', 'month': 'akmonthly', 'minute': 'akmin'};
-export const INDEX_LABELS = ['sh', 'sz', 'hs300', 'sz50', 'cyb', 'zxb', 'zx300', 'zh500'];
-export const INDEX_LIST = {'sh': 'sh000001', 'sz': 'sz399001', 'hs300': 'sz399300',
-  'sz50': 'sh000016', 'zxb': 'sz399005', 'cyb': 'sz399006', 'zx300': 'sz399008', 'zh500': 'sh000905'};
+import moment from 'moment';
 
+export const K_TYPE = {
+  day: 'akdaily',
+  week: 'akweekly',
+  month: 'akmonthly',
+  minute: 'akmin',
+};
+
+export const INDEX_LABELS = ['sh', 'sz', 'hs300', 'sz50', 'cyb', 'zxb', 'zx300', 'zh500'];
+
+export const INDEX_LIST = {
+  sh: 'sh000001',
+  sz: 'sz399001',
+  hs300: 'sz399300',
+  sz50: 'sh000016',
+  zxb: 'sz399005',
+  cyb: 'sz399006',
+  zx300: 'sz399008',
+  zh500: 'sh000905',
+};
+
+export const DATE_NOW = moment().format('YYYY-MM-DD');
